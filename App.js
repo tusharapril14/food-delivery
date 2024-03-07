ab9b8ae67181c7cc4621bad1397ca8b5b36d5260
Tushar <tushar.verma@webshar.org>
2024-03-07 14:20:17 +0530

    React using jsx

--- App.js
+++ App.js
@@ -2,25 +2,8 @@ import  React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-const element = React.createElement("div",
-    {id:"parent"},[
-        React.createElement("div",{id:"child"},[
-            React.createElement("h1",{id:"h1"},"Hello H1"),
-            React.createElement("h2",{id:"child"},"Hello H1")
-        ]),
-        React.createElement("div",{id:"child2"},[
-            React.createElement("h1",{id:"h1"},"Hello H1"),
-            React.createElement("h2",{id:"child"},"Hello H1")
-        ]),
-    ]
-
-);
-
-
-
+const element = <h1 id='heading' className='test'>Hello React Js using jsx.</h1>
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-const root2 = ReactDOM.createRoot(document.getElementById('root2'))
 root.render(element)
-root2.render(element)
