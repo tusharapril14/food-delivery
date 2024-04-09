a5bd78aa4848cb71cf7d2af44307a04b26d099b1
Tushar <tushar.verma@webshar.org>
2024-04-09 23:34:39 +0530

    minorfx

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -27,10 +27,6 @@ const Restaurants = ()=>{
             <div class="w-full p-4 text-center bg-white border border-gray-200 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                 <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Meghna foods</h5>
                 <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400"></p>
-                <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
-                <div class="flex mt-2.5 mb-5 justify-between">
-        </div>
-                </div>
             </div>
         </div>
     )
